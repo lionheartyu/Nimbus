123f8e4856965ba4133cc2a131787daa8574468d
lion <545258830>
2025-12-24 10:41:53 +0800

    update cmake main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include "server/server.h"
 #include "server/file_server.h"
 #include <aws/core/Aws.h>
 
@@ -9,7 +8,7 @@ int main() {
     {
         // 文件传输服务器 3线程
         EventLoop loop;
-        InetAddress addr(8080, "192.168.122.164");
+        InetAddress addr(8080, "10.20.32.88");
         FileServer server(&loop, addr, "file-server");
         server.start();
         loop.loop();

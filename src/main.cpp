7865038ca35ee809b3a816cd4657be41e6dcbc01
lion <545258830>
2025-12-26 12:13:34 +0800

    update-cm

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,20 @@
 #include <aws/core/Aws.h>
 
 int main() {
+    // 初始化 AWS/MinIO SDK
     Aws::SDKOptions options;
-    Aws::InitAPI(options); // 初始化 AWS/MinIO SDK
+    Aws::InitAPI(options);
 
     {
-        // 文件传输服务器 3线程
+        // 创建事件循环和服务器对象，监听 10.20.32.88:8080，3线程
         EventLoop loop;
         InetAddress addr(8080, "10.20.32.88");
         FileServer server(&loop, addr, "file-server");
         server.start();
-        loop.loop();
+        loop.loop(); // 启动事件循环
     }
 
-    Aws::ShutdownAPI(options); // 关闭 AWS/MinIO SDK
+    // 关闭 AWS/MinIO SDK
+    Aws::ShutdownAPI(options);
     return 0;
 }
